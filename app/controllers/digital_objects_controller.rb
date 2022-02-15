cb2599960752fedf2ef4febecf6e01a865ac8b62
Katherine Perdue <abhyastamita@hotmail.com>
2022-02-14 19:47:00 -0500

    Search form working

--- app/controllers/digital_objects_controller.rb
+++ app/controllers/digital_objects_controller.rb
@@ -4,23 +4,20 @@ class DigitalObjectsController < ApplicationController
     if (!params[:query].blank?)
       @collection = DigitalObject.search(params[:query])
       if (@collection)
-        # respond_to do |format|
-        #   format.js {render partial: 'users/result'}
-        # end
-        render json: @collection
+        respond_to do |format|
+           format.js {render partial: 'users/result'}
+        end
       else
-        flash[:alert] = "Your search query returned no results."
-        # respond_to do |format|
-        #   flash.now[:alert] = "Please enter a valid symbol to search"
-        #   format.js {render partial: 'users/result'}
-        # end
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid symbol to search"
+          format.js {render partial: 'users/result'}
+        end
       end
     else
-      flash.now[:alert] = "Please enter a term to search for."
-      # respond_to do |format|
-      #   flash.now[:alert] = "Please enter a symbol to search"
-      #   format.js {render partial: 'users/result'}
-      # end
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a symbol to search"
+        format.js {render partial: 'users/result'}
+      end
     end
   end
 
